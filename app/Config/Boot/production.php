f0c59cedae115234b0f14153f2c92a74cdb7f79a
Cristian-Reyes-007 <crisreyesc26@gmail.com>
2025-02-17 22:05:34 -0600

    Subida del proyecto al repositorio

--- app/Config/Boot/production.php
+++ app/Config/Boot/production.php
@@ -13,7 +13,8 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 // If you want to suppress more types of errors.
 // error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 ini_set('display_errors', '1');
-//1= muestra errores 0= no muestra errores
+//1= muestra errores
+//0=no muestra errores
 
 /*
  |--------------------------------------------------------------------------
