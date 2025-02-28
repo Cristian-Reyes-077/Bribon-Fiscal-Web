613a7a5069ecb98b116de98a318952706d2ddad0
Cristian Guadalupe Reyes Contreras <crisreyesc26@gmail.com>
2025-02-28 12:21:34 -0600

    Creacion controller y models auth

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,11 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
-
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'AuthenticationController::index');
+$routes->get('/', 'AuthenticationController::index');
+
+
+
